b7f2241e5cf779c14701ea3e1b8e5447043ca989
Mark Wong <fmarkwong@gmail.com>
2020-01-01 19:34:22 -0800

    Save password hash

--- lib/chronos/accounts/credential.ex
+++ lib/chronos/accounts/credential.ex
@@ -5,6 +5,7 @@ defmodule Chronos.Accounts.Credential do
   schema "credentials" do
     field :email, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
 
     belongs_to :user, Chronos.Accounts.User
 
@@ -14,8 +15,17 @@ defmodule Chronos.Accounts.Credential do
   @doc false
   def changeset(credential, attrs) do
     credential
-    |> cast(attrs, [:email, :password_hash])
-    |> validate_required([:email])
+    |> cast(attrs, [:email, :password_hash, :password])
+    |> validate_required([:email, :password])
     |> unique_constraint(:email)
+    |> put_password_hash()
   end
+
+  defp put_password_hash(
+         %Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset
+       ) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
